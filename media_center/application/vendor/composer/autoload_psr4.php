4fcee50768371367a7f02143662e0d3c707a47ce
hnguyen8909 <hoang.nguyen@pricepony.com.ph>
2016-04-20 18:39:35 +0700

    Fix composer

--- media_center/application/vendor/composer/autoload_psr4.php
+++ media_center/application/vendor/composer/autoload_psr4.php
@@ -7,6 +7,7 @@ $baseDir = dirname(dirname($vendorDir));
 
 return array(
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
     'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
     'React\\Promise\\' => array($vendorDir . '/react/promise/src'),
     'GuzzleHttp\\Stream\\' => array($vendorDir . '/guzzlehttp/streams/src'),
--- media_center/application/vendor/composer/autoload_real.php
+++ media_center/application/vendor/composer/autoload_real.php
@@ -23,34 +23,24 @@ class ComposerAutoloaderInitd3edcf91f152f54bb12c80dcee46ddc0
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitd3edcf91f152f54bb12c80dcee46ddc0', 'loadClassLoader'));
 
-        if (PHP_VERSION_ID >= 50600) {
-            require_once __DIR__ . '/autoload_static.php';
+        $map = require __DIR__ . '/autoload_namespaces.php';
+        foreach ($map as $namespace => $path) {
+            $loader->set($namespace, $path);
+        }
 
-            call_user_func(\Composer\Autoload\ComposerStaticInitd3edcf91f152f54bb12c80dcee46ddc0::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
+        $map = require __DIR__ . '/autoload_psr4.php';
+        foreach ($map as $namespace => $path) {
+            $loader->setPsr4($namespace, $path);
+        }
 
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
+        $classMap = require __DIR__ . '/autoload_classmap.php';
+        if ($classMap) {
+            $loader->addClassMap($classMap);
         }
 
         $loader->register(true);
 
-        if (PHP_VERSION_ID >= 50600) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitd3edcf91f152f54bb12c80dcee46ddc0::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
+        $includeFiles = require __DIR__ . '/autoload_files.php';
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequired3edcf91f152f54bb12c80dcee46ddc0($fileIdentifier, $file);
         }
