d0857e5d56aaaea2bb2ca0a766d48470ca5376c0
Kang <ptnhuan@gmail.com>
2016-05-04 22:42:32 +0700

    dddd
    ddddddddddddd

--- media_center/public/themes/media/parts/widgets/header/logo.php
+++ media_center/public/themes/media/parts/widgets/header/logo.php
@@ -1,8 +1,6 @@
 <!-- ============================================================= LOGO ============================================================= -->
 <div class="logo">
-	<a href="<?php echo BASE_URL;?>/index.php?page=home">
-		<!--<img alt="logo" src="assets/images/logo.svg" width="233" height="54"/>-->
-		<!--<object id="sp" type="image/svg+xml" data="assets/images/logo.svg" width="233" height="54"></object>-->
+	<a href="<?php echo BASE_URL;?>">
                 <img src="<?php echo base_url() ?>logo.png">
 	</a>
 </div><!-- /.logo -->
