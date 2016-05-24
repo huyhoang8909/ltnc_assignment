32a2f527760db48a159573df104ec941e3ea737e
Kang <ptnhuan@gmail.com>
2016-05-24 12:02:01 +0700

    aaa
    aaaa

--- media_center/public/themes/media/parts/section/home-page-tabs.php
+++ media_center/public/themes/media/parts/section/home-page-tabs.php
@@ -33,7 +33,7 @@
 
                                     <div class="hover-area">
                                         <div class="add-cart-button">
-                                            <a href="index.php?page=single-product" class="le-button">add to cart</a>
+                                            <a href="<?php echo base_url('cart/add/'.$item->ITEM_ID) ?>" class="le-button">add to cart</a>
                                         </div>
                                         <div class="wish-compare">
                                             <a class="btn-add-to-wishlist" href="#">add to wishlist</a>
