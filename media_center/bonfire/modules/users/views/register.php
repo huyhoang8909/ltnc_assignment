dfcf34a0bbb48ed8a53ee1170137a852cf527f29
Hoang Nguyen <huyhoang8909@gmail.com>
2016-05-25 22:07:55 +0700

    update login

--- media_center/bonfire/modules/users/views/register.php
+++ media_center/bonfire/modules/users/views/register.php
@@ -44,7 +44,7 @@ $fieldData = array(
                     Events::trigger('render_user_form');
                     ?>
                     <!-- Start of User Meta -->
-                    <?php //$this->load->view('users/user_meta', array('frontend_only' => true)); ?>
+                    <?php $this->load->view('users/user_meta', array('frontend_only' => true)); ?>
                     <!-- End of User Meta -->
                 </fieldset>
                 <br/>
